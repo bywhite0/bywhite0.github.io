f59f3201605d165574c185dece0288e31a5f9a02
白影White <3143027430@qq.com>
2021-11-14 00:16:01 +0800

    部落格新增留言板

--- blog/config.js
+++ blog/config.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('/sw.js')
       };
-Fancybox.bind('img');
+Fancybox.bind('img[data-fancybox]');
 new Docute({
         target: '#app',
 darkThemeToggler: true,
@@ -15,7 +15,17 @@ nav: [
       link: 'https://github.com/bywhite0/bywhite0.github.io'
     }
   ],
-footer: `<a href="https://bywhite0.github.io">鸽子窝的二楼</a>｜<a href="https://bywhite.vercel.app">平行世界的二楼</a>｜<a href="https://github.com/bywhite0/bywhite0.github.io">二楼仓库</a>`,
+footer: `<a href="https://bywhite0.github.io">鸽子窝的二楼</a>｜<a href="https://bywhite.vercel.app">平行世界的二楼</a>｜<a href="https://github.com/bywhite0/bywhite0.github.io">二楼仓库</a><div id="wl" class="docute-footer"></div><v-script>new MiniValine({
+			el: '#wl',
+			backend: 'waline',
+			serverURLs: 'https://bywl.vercel.app',
+			mode: 'DesertsP',
+			placeholder: '来发评论吧~',
+			avatarUrl: 'https://gravatar.loli.net/avatar',
+			barrager: 2,
+			emoticonUrl: ['https://cdn.jsdelivr.net/gh/MiniValine/bilibilis@latest', 'https://cdn.jsdelivr.net/npm/alus@latest', 'https://cdn.jsdelivr.net/gh/MiniValine/MiniValine/imgs/funny', 'https://cdn.jsdelivr.net/gh/MiniValine/tieba@latest', 'https://cdn.jsdelivr.net/gh/MiniValine/twemoji@latest', 'https://cdn.jsdelivr.net/gh/MiniValine/weibo@latest', 'https://cdn.jsdelivr.net/gh/lrhtony/BiliEmoji@master/src/热词系列一','https://cdn.jsdelivr.net/gh/walinejs/emojis@1.0.0/bilibili','https://cdn.jsdelivr.net/gh/walinejs/emojis@1.0.0/qq','https://cdn.jsdelivr.net/gh/lrhtony/BiliEmoji@master/src/洛天依','https://cdn.jsdelivr.net/gh/lrhtony/BiliEmoji@master/src/2020拜年祭','https://cdn.jsdelivr.net/gh/lrhtony/BiliEmoji@master/src/2233白色情人节表情包','https://cdn.jsdelivr.net/gh/lrhtony/BiliEmoji@master/src/泠鸢yousa','https://cdn.jsdelivr.net/gh/lrhtony/BiliEmoji@master/src/阴阳师·平安物语','https://cdn.jsdelivr.net/gh/lrhtony/BiliEmoji@master/src/原神','https://cdn.jsdelivr.net/gh/lrhtony/BiliEmoji@master/src/时光代理人', ]
+		});</v-script>
+	`,
 sidebar: [
     // A sidebar item, with child links
     {
@@ -44,11 +54,11 @@ sidebar: [
       children: [
         {
           title: '鸽子窝の变迁',
-          link: '/1f'
+          link: '/log/1f'
         },
         {
           title: '二楼装修记',
-          link: '/2f'
+          link: '/log/2f'
         }
      ]
     },
